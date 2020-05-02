d0fc880b11b473045b4f9db6a6294803b40b2633
UzairLakhani <lakhaniuzair780@gmail.com>
2020-05-01 21:22:49 -0400

    Add files via upload

---
+++ src/Bonus.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nanu
+ */
+public class Bonus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Question name = new Question("Uzair");
+        
+        System.out.println("My name is " + name.getQuestions());
+        
+    }
+    
+}
+}
---
+++ src/Question.java
@@ -0,0 +1,12 @@
+public class Question {
+
+    private String name;
+    
+    public Question (String name){
+        this.name = name;
+    }
+    
+    public String getQuestions(){
+        return name;
+    }
+}
