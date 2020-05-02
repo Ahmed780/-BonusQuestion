225f5565b829f08953602df6ee48ef0b36d3f682
UzairLakhani <lakhaniuzair780@gmail.com>
2020-05-01 21:56:24 -0500

    Update Question.java

--- src/Question.java
+++ src/Question.java
@@ -3,7 +3,7 @@ public class Question {
     private String name;
     
     public Question (String name){
-        this.name = name;
+        this.name =  name;
     }
     
     public String getQuestions(){
